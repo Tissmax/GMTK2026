7483eed2ccd9fbf19bd0756e4af201bfbd2052bc
Tissmax <mathis.basier@gmail.com>
2026-07-23 16:08:37 +0200

    Ajout du timer visuel

--- gmtk-2026/entities/ennemy.gd
+++ gmtk-2026/entities/ennemy.gd
@@ -3,6 +3,10 @@ class_name Ennemy extends StaticBody2D
 @export var data: EnnemyData
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var timer: Timer = $Timer
+@onready var loader: TextureProgressBar = $Loader
+
+func _process(delta: float) -> void:
+	loader.value = (timer.time_left / data.time_to_live) * 100
 
 func _ready() -> void:
 	sprite_2d.texture = data.sprite
@@ -13,7 +17,6 @@ func _ready() -> void:
 	
 	timer.connect("timeout", _enemy_timeout)
 	
-	data.add_letters(letters)
 
 func _enemy_timeout():
 	queue_free()
